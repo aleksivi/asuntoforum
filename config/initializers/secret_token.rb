12f59660e968b05959a2f2bbb90dd11def1ef7fd
Aleksi <alex2209@luukku.com>
2011-09-26 18:21:55 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asuntoforum::Application.config.secret_token = 'b08cdaffda8a59681ef83124476f02942e63db022f457dee2a9aac1364cfdbe35eab5c7ab0b270e4ba39622d2f98a66f07d9422583b1f062e1d32a7f23d6a72a'
